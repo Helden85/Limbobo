ae2636697e066c944f48fb096f35dd7f6014c3cc
skillzz123jj <turpeinenmila6@gmail.com>
2023-03-31 22:33:09 +0300

    Added game over scene

--- Assets/Scenes/Mila/Scripts/SceneChanger.cs
+++ Assets/Scenes/Mila/Scripts/SceneChanger.cs
@@ -5,20 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-
+    [SerializeField] int scene;
+    
     void Start()
     {
 
     }
 
-
+   
     void Update()
     {
 
     }
+
     //Changes the scene by scene number
     public void ChangeScene(int scene)
     {
         SceneManager.LoadScene(scene);
     }
+    //Changes the scene on collision according to the tag
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerCollider"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
 }
