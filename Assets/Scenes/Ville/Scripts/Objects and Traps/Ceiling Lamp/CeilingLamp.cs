0541608df563a1d89d3763f9d072a4ca8e029820
VilleK88 <ville.karppinen88@gmail.com>
2023-04-17 15:58:26 +0300

    T

---
+++ Assets/Scenes/Ville/Scripts/Objects and Traps/Ceiling Lamp/CeilingLamp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingLamp : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scenes/Ville/Scripts/Player/PlayerController.cs
@@ -0,0 +1,196 @@
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Other Player Parameters")]
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private CapsuleCollider2D capsuleCollider;
+
+    [Header("Speed and Jump Parameters")]
+    float moveSpeed = -10;
+    float jumpForce = 15;
+    float jumpTimes = 0;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+
+    [Header("Attack Parameters")]
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    //[SerializeField] private float damage = 1;
+    float attackMaxTime = 0.25f;
+    float attackCounter = 0;
+    public bool attacking = false;
+
+    [Header("Blocking Parameters")]
+    public bool blocking = false;
+    float blockDuration = 0.5f;
+    public float blockTimer;
+
+    [Header("Combo Parameters")]
+    float lastAttackMaxTime = 0.5f;
+    public float lastAttackTimer = 0;
+    bool lastAttackBool = false;
+    public bool attackTwo = false;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+
+        Attack();
+        if (lastAttackBool)
+        {
+            if (lastAttackTimer < lastAttackMaxTime)
+            {
+                lastAttackTimer += Time.deltaTime;
+            }
+            else
+            {
+                lastAttackTimer = 0;
+                lastAttackBool = false;
+            }
+        }
+
+        Block();
+
+        Debug.Log("is on wall " + onWall());
+        Debug.Log("is on ground " + isGrounded());
+    }
+
+    void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") * moveSpeed;
+        anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        rb2d.velocity = new Vector2(horizontalInput, rb2d.velocity.y);
+
+        if(horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded() || jumpTimes < 1)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+                jumpTimes += 1;
+            }
+            else if (onWall() || jumpTimes < 1)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+                jumpTimes += 1;
+            }
+        }
+
+
+        if (isGrounded())
+        {
+            jumpTimes = 0;
+        }
+
+        anim.SetBool("Grounded", isGrounded());
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(capsuleCollider.bounds.center,
+            capsuleCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(capsuleCollider.bounds.center,
+            capsuleCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        if (Input.GetKeyDown(KeyCode.RightControl) && Time.time > attackCounter + attackMaxTime)
+        {
+            attacking = true;
+            attackCounter = Time.time;
+
+            if (lastAttackTimer == 0)
+            {
+                //anim.SetTrigger("Attack1");
+                lastAttackBool = true;
+
+                foreach (Collider2D enemy in hitEnemies)
+                {
+                    enemy.GetComponent<Health>().TakeDamage(1);
+                }
+            }
+            else
+            {
+                //anim.SetTrigger("Attack2");
+
+                lastAttackTimer = 0;
+                lastAttackBool = false;
+                attackTwo = true;
+
+                foreach (Collider2D enemy in hitEnemies)
+                {
+                    enemy.GetComponent<Health>().TakeDamage(2);
+                }
+            }
+        }
+        else
+        {
+            attacking = false;
+            attackTwo = false;
+        }
+    }
+
+    public void Block()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            //anim.SetTrigger("Block");
+            blocking = true;
+            blockTimer = blockDuration;
+        }
+
+        if (blocking)
+        {
+            blockTimer -= Time.deltaTime;
+            if (blockTimer <= 0f)
+            {
+                blocking = false;
+            }
+        }
+
+        Debug.Log(blocking);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
