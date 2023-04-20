0cf8d48ff5ed4d141a2f84a59228f300aa51aa42
VilleK88 <ville.karppinen88@gmail.com>
2023-04-20 13:43:50 +0300

    Moving Platform
    Toimiva liikkuva alusta, jonka voi asettaa horisontaalisesti liikkuvaksi tai vertikaalisesti boolean asetuksesta.

--- Assets/Scenes/Ville/Scripts/Objects and Traps/MovingPlatform.cs
+++ Assets/Scenes/Ville/Scripts/Objects and Traps/MovingPlatform.cs
@@ -42,4 +42,20 @@ public class MovingPlatform : MonoBehaviour
         Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
         Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
 }
