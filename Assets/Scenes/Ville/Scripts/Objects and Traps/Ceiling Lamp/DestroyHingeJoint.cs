e6d265e7fb9bc33ac40bc2e1e7d9ae2330cc751d
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2023-04-20 08:10:17 +0300

    Revert "T"
    This reverts commit b08895f1fe315184b334d3e16527ab54c4be90d6.

---
+++ Assets/Scenes/Ville/Scripts/Objects and Traps/Ceiling Lamp/DestroyHingeJoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHingeJoint : MonoBehaviour
+{
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
