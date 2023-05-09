71d0b2b4f9fb9e59a8d1a4848b7e664b4bb6c196
VilleK88 <ville.karppinen88@gmail.com>
2023-05-09 10:26:32 +0300

    Testiscenessä vihollisen tekoa
    Harjoitusvihollisen tekoa hitBoxilla, jota voi mahdollisesti hyödyntää skeleton rigging kanssa, jotta vihollinen voi tehdä vahinkoa pelaajalle.

---
+++ Assets/Scenes/Ville/Scripts/Enemies/Enemy_behaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_behaviour : MonoBehaviour
+{
+    #region Public Variables
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance;
+    public float moveSpeed;
+    public float timer;
+    #endregion
+
+    void Update()
+    {
+        
+    }
+}
